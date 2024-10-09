645befe26534c70609114d9bf413a3494764a029
KiraniaKharisa <kiraniakharisasuyatno@gmail.com>
2024-10-09 11:53:35 +0700

    Dari Kharisa Membenahi Text Dan Responsive

--- src/script.js
+++ src/script.js
@@ -5,3 +5,23 @@ hamburgerButton.addEventListener("click", () => {
     navbar.classList.toggle("active");
     hamburgerButton.classList.toggle("diklik");
 })
+
+// Navbar Section Active
+const allSection = document.querySelectorAll("section"),
+allLinksNavbar = document.querySelectorAll(".nav-link");
+
+window.onscroll = () => {
+    allSection.forEach(section => {
+        let top = window.scrollY,
+        sectionOffset = section.offsetTop - 100,
+        heightSection = section.offsetHeight,
+        idSection = section.getAttribute('id');
+        
+        if(top >= sectionOffset && top < sectionOffset + heightSection) {
+            allLinksNavbar.forEach(link => {
+                link.classList.remove("active");
+                document.querySelector(".nav-items-links a[href*=" + idSection + "]").classList.add("active");
+            })
+        }
+    })
+}
