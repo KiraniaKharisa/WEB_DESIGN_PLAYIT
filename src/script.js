7411cea92f87d5e0f510b3f973a738090172600a
DewaPermana22 <dewarisa77@gmail.com>
2024-10-30 22:25:18 +0700

    Membuat Sedikit Animasi, 30 OKT 2024

--- src/script.js
+++ src/script.js
@@ -47,25 +47,6 @@ window.addEventListener("load", () => {
   document.querySelector(".loading").style.display = "none";
 })
 
-// document.addEventListener("DOMContentLoaded", () =>{
-//   gsap.registerPlugin(ScrollTrigger);
-//   gsap.to(".animasi",{
-//     scrollTrigger: {
-//       trigger: ".divAnimasiCard",    // Section yang memicu animasi
-//       start: "top center", 
-//       end: "bottom center",           // Memulai animasi saat bagian atas section masuk ke tengah viewport
-//       toggleActions: "play none none reset", // Memulai animasi saat memasuki section, dan reset saat keluar
-//       markers: true                   // Marker untuk melihat posisi start dan end (opsional, hanya untuk debugging)
-//   },
-//     opacity: 1,
-//     y: 0,
-//     duration: 1.5,
-//     stragger : 0.3,
-//     ease : "power2.out",
-//   })
-    
-// })
-
 let kaliDuration = 0.5;
 document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger);
@@ -84,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
                   start: "top center",       // Mulai animasi ketika elemen hampir terlihat
                   end: "bottom top",      // Akhiri animasi saat elemen meninggalkan viewport
                   toggleActions: "play none none reverse", // Balik ke posisi awal saat keluar
-                  markers: true,         // Tambahkan ini untuk debugging
+                  markers: false,         // Tambahkan ini untuk debugging
                   onEnter: () => gsap.to(card, { opacity: 1, y: 0, duration: 1 * (index + kaliDuration) }),  // Animasi saat masuk
                   onEnterBack: () => gsap.to(card, { opacity: 1, y: 0, duration: 1 * (index + kaliDuration) }),
                   onLeave: () => gsap.to(card, { opacity: 0, y: -100, duration: 1 * (index + kaliDuration) }),
@@ -94,3 +75,152 @@ document.addEventListener("DOMContentLoaded", () => {
       );
   });
 });
+
+// animasi deskripsi
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray(".animasi-deskripsi").forEach((card, index) => {
+      const direction = index % 2 === 0 ? -100 : 100;
+
+      gsap.fromTo(card,
+          { opacity: 0, x: direction },
+          {
+              opacity: 1,
+              x: 0,
+              duration: 1.5,
+              ease: "power2.out",
+              scrollTrigger: {
+                  trigger: card,
+                  start: "top center",         
+                  end: "bottom top",
+                  toggleActions: "play none none reverse",
+                  markers: false
+              }
+          }
+      );
+  });
+});
+
+
+// animasi judul
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".judul-1",{
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: ".judul-1",
+      start: "top center",
+      end: "bottom top",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  })
+})
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".judul-2",{
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: ".judul-2",
+      start: "top center",
+      end: "bottom top",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  })
+})
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".judul-3",{
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: ".judul-3",
+      start: "top center",
+      end: "bottom top",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  })
+})
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".judul-4",{
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: ".judul-4",
+      start: "top center",
+      end: "bottom top",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  })
+})
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".judul-5",{
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: ".judul-5",
+      start: "top center",
+      end: "bottom top",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  })
+})
+
+kaliDuration = 0.5;
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Untuk setiap card, buat animasi 'while in view'
+  gsap.utils.toArray(".animasi-sejarah-des").forEach((card, index) => {
+      gsap.fromTo(card, 
+          { opacity: 0, x: -100 },    // Posisi awal
+          { 
+              opacity: 1,
+              x: 0,
+              duration: 1 * (index + kaliDuration),
+              ease: "power2.out",
+              scrollTrigger: {
+                  trigger: card,
+                  start: "top center",       // Mulai animasi ketika elemen hampir terlihat
+                  end: "bottom top",      // Akhiri animasi saat elemen meninggalkan viewport
+                  toggleActions: "play none none reverse", // Balik ke posisi awal saat keluar
+                  markers: false,         // Tambahkan ini untuk debugging
+                  onEnter: () => gsap.to(card, { opacity: 1, x: 0, duration: 1 * (index + kaliDuration) }),  // Animasi saat masuk
+                  onEnterBack: () => gsap.to(card, { opacity: 1, x: 0, duration: 1 * (index + kaliDuration) }),
+                  onLeave: () => gsap.to(card, { opacity: 0, x: -100, duration: 1 * (index + kaliDuration) }),
+                  onLeaveBack: () => gsap.to(card, { opacity: 0, x: -100, duration: 1 * (index + kaliDuration) })// Reset saat keluar
+              }
+          }
+      );
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".img-sjarah",{
+    opacity: 1,
+    x: 0,
+    duration: 2,
+    scrollTrigger: {
+      trigger: ".img-sjarah",
+      start: "top center",
+      end: "bottom top",
+      toggleActions: "play none none reverse",
+      markers: false
+    }
+  })
+})
