03a3efb741029e0c1a7cb711dea3729b0f3a4813
KiraniaKharisa <kiraniakharisasuyatno@gmail.com>
2024-10-30 14:00:43 +0700

    Dari couple 30 okt 2024

--- src/testimonial.js
+++ src/testimonial.js
@@ -1,3 +1,8 @@
 window.addEventListener("load", () => {
     document.querySelector(".loading").style.display = "none";
 })
+
+const textArea = document.querySelector(".textArea");
+textArea.addEventListener("input", () => {
+    document.querySelector(".textValueArea span").textContent = textArea.value.length;
+})
